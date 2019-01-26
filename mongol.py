0d8ac7c1da3a8c677779f8dbe78ade307d0cd73b
prozaqq <shaishnaider@gmail.com>
2019-01-26 07:57:14 +0200

    added argparse for single network scan or file scan and moved each scan type to a different method. added argparse for verbosity

--- mongol.py
+++ mongol.py
@@ -5,7 +5,7 @@ import argparse
 import pymongo
 
 
-def save(filename,str,ip):
+def save(filename, str, ip):
     with open(filename,'a+') as file:
         file.write('Host - {}\n\n'.format(ip))
         file.write('{}\n\n'.format(str))
@@ -13,33 +13,52 @@ def save(filename,str,ip):
 
 def check_args(args=None):
     parser = argparse.ArgumentParser(description='A tool to enumerate MongoDB databases without authentication')
-    parser.add_argument('-l','--log',action='store_true')
-    parser.add_argument('-o','--output',action='store_true')
+    parser.add_argument('-l', '--log', action='store_true',help='log exceptions into a text file')
+    parser.add_argument('-o', '--output', action='store_true', help='save successful enumerations to a text file')
+    parser.add_argument('-f', '--file', help='network list file path', default='')
+    parser.add_argument('-net', '--network', help='network to scan / for example \"192.168.0.1/24\"', default='')
+    parser.add_argument('-v', '--verbose', action='store_true', help='prints exceptions to screen')
 
     results = parser.parse_args(args)
-    return(results.log, results.output)
+    return results.log, results.output, results.file, results.network, results.verbose
+
+
+def check_open(hosts):
+    for host in hosts:
+        if host not in ip_list and nm[host]['tcp'][27017]['state'] == 'open':
+            ip_list.append(host)
+            print('[+] {} - {} / open'.format(host, PORT))
+
+
+def scan_file(file):
+    print('[!] Starting a network scan from a file ...')
+    cidr_list = open(filename).read().splitlines()
+    for cidr in cidr_list:
+        print('[!] Scanning - {}'.format(cidr))
+        nm.scan(cidr, PORT)
+        check_open(nm.all_hosts())
+
+
+def scan_network(network):
+    print('[!] Starting a single network scan ...')
+    print('[!] Scanning - {}'.format(network))
+    nm.scan(network, PORT)
+    check_open(nm.all_hosts())
 
 
 if __name__ == "__main__":
 
-    log , output = check_args(sys.argv[1:])
+    log, output, filename, network, verbose = check_args(sys.argv[1:])
 
     PORT = '27017'
 
-    cidr_list = open('ipblock.txt').read().splitlines()
     ip_list = []
     nm = nmap.PortScanner()
 
-    # Scan
-
-    for cidr in cidr_list:
-        print('Scanning - {}'.format(cidr))
-        nm.scan(cidr, PORT)
-
-    for host in nm.all_hosts():
-        if host not in ip_list and nm[host]['tcp'][27017]['state'] == 'open':
-            ip_list.append(host)
-            print('[+] {} - {} / open'.format(host, PORT))
+    if len(filename) > len(network):
+        scan_file(filename)
+    else:
+        scan_network(network)
 
     # Connect and enumerate dbs and collections
 
@@ -61,5 +80,7 @@ if __name__ == "__main__":
                 pymongo.errors.ServerSelectionTimeoutError) as e:
             if log:
                 save('log.txt', e, ip)
+            if verbose:
+                print('[!] {}'.format(e))
         client.close()
 
